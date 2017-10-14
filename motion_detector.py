23314dd9fc4aa42812643d135eabb1eb9b72b59d
timyrka67 <timyrka67@gmail.com>
2017-10-14 23:08:28 +0300

    Add bigest object tracking

--- motion_detector.py
+++ motion_detector.py
@@ -8,6 +8,7 @@ import datetime
 import imutils
 import time
 import cv2
+from collections import deque
 import threading
 import thread
 import numpy as np
@@ -21,33 +22,26 @@ def update(time):
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+ap.add_argument("-a", "--min-area", type=int, default=800, help="minimum area size")
 args = vars(ap.parse_args())
 firstFrame = None
+buffer = 32
+pts = deque(maxlen=buffer)
 print cv2.__version__
+counter = 0
+(dX, dY) = (0, 0)
+direction = ""
 
 def run():
-    # if the video argument is None, then we are reading from webcam
-    global args
-    global firstFrame
+    global direction, dX, dY, firstFrame, args, counter
     if args.get("video", None) is None:
         camera = cv2.VideoCapture(1)
         time.sleep(0.25)
-
-    # otherwise, we are reading from a video file
     else:
         camera = cv2.VideoCapture(args["video"])
-
-    # initialize the first frame in the video stream
-    # loop over the frames of the video
     while True:
-        # grab the current frame and initialize the occupied/unoccupied
-        # text
         (grabbed, frame) = camera.read()
         text = "Unoccupied"
-
-        # if the frame could not be grabbed, then we have reached the end
-        # of the video
         if not grabbed:
             break
 
@@ -56,31 +50,33 @@ def run():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-        # if the first frame is None, initialize it
         if firstFrame is None:
             firstFrame = gray
             continue
 
-        # compute the absolute difference between the current frame and
-        # first frame
         frameDelta = cv2.absdiff(firstFrame, gray)
         thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
-        # dilate the thresholded image to fill in holes, then find contours
-        # on thresholded image
+        # dilate the thresholded image to fill in holes, then find contours on thresholded image
         thresh = cv2.dilate(thresh, None, iterations=2)
+        #CHAIN_APPROX_TC89_L1, CHAIN_APPROX_TC89_KCOS, CHAIN_APPROX_SIMPLE, CHAIN_APPROX_NONE
         _, cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        # loop over the contours
+        if len(cnts) > 0:
+            c = max(cnts, key=cv2.contourArea)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            pts.appendleft(center)
+
+        for i in np.arange(1, len(pts)):
+            if pts[i - 1] is None or pts[i] is None:
+                continue
+            thickness = int(np.sqrt(buffer / float(i + 1)) * 2.5)
+            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 0), thickness)
+
         for c in cnts:
-            # if the contour is too small, ignore it
             if cv2.contourArea(c) < args["min_area"]:
                 continue
-
-            # compute the bounding box for the contour, draw it on the frame,
-            # and update the text
-            #(x, y, w, h) = cv2.boundingRect(c)
-            #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             rect = cv2.minAreaRect(c)
             box = cv2.boxPoints(rect)
             box = np.int0(box)
@@ -88,23 +84,19 @@ def run():
 
             text = "Occupied"
 
-        # draw the text and timestamp on the frame
         cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
         cv2.putText(frame, datetime.datetime.now().strftime("%d/%m/%y %I:%M:%S%p"),
             (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
         # show the frame and record if the user presses a key
-        cv2.imshow("Security Feed", frame)
+        cv2.imshow("Feed", frame)
         cv2.imshow("Thresh", thresh)
         cv2.imshow("Frame Delta", frameDelta)
         key = cv2.waitKey(1) & 0xFF
 
-        # if the `q` key is pressed, break from the lop
         if key == ord("q"):
             break
-
-    # cleanup the camera and close any open windows
     camera.release()
     cv2.destroyAllWindows()
 
