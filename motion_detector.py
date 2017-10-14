6a5c9b6b7f1e31fb146d285c0830e0233329829a
timyrka67 <timyrka67@gmail.com>
2017-10-14 17:32:24 +0300

    Update

--- motion_detector.py
+++ motion_detector.py
@@ -10,6 +10,7 @@ import time
 import cv2
 import threading
 import thread
+import numpy as np
 
 def update(time):
   print "Frame updated "
@@ -23,6 +24,7 @@ ap.add_argument("-v", "--video", help="path to the video file")
 ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
 args = vars(ap.parse_args())
 firstFrame = None
+print cv2.__version__
 
 def run():
     # if the video argument is None, then we are reading from webcam
@@ -54,9 +56,6 @@ def run():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-
-        #update()
-
         # if the first frame is None, initialize it
         if firstFrame is None:
             firstFrame = gray
@@ -70,8 +69,7 @@ def run():
         # dilate the thresholded image to fill in holes, then find contours
         # on thresholded image
         thresh = cv2.dilate(thresh, None, iterations=2)
-        (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE)
+        _, cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         # loop over the contours
         for c in cnts:
@@ -81,14 +79,19 @@ def run():
 
             # compute the bounding box for the contour, draw it on the frame,
             # and update the text
-            (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            #(x, y, w, h) = cv2.boundingRect(c)
+            #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            rect = cv2.minAreaRect(c)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
+
             text = "Occupied"
 
         # draw the text and timestamp on the frame
         cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+        cv2.putText(frame, datetime.datetime.now().strftime("%d/%m/%y %I:%M:%S%p"),
             (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
         # show the frame and record if the user presses a key
@@ -107,7 +110,7 @@ def run():
 
 
 try:
-    thread.start_new_thread(update, (30,)) ##update every 30 sec
+    thread.start_new_thread(update, (1000,)) ##update every 30 sec
     thread.start_new_thread(run, ())
 
 except:
